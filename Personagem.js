126eb6b976113f84866a740941224658f2e78bd3
Cassio Urtiga <cassiourtiga190@gmail.com>
2022-12-07 18:16:23 -0300

    Adicionada a movimentação do personagem

--- Personagem.js
+++ Personagem.js
@@ -1,52 +1,80 @@
 class Personagem{
 
-    constructor(caneta){
+    constructor(caneta, teclado){
         this.caneta = caneta;
+        this.teclado = teclado;
+
         this.img = new Image();
         this.img.src = "sprites/avatar.png";
 
-        this.x;
-        this.y;
-        this.tam;
-        this.atrasoSprite;
-        this.contadorAtraso = 0;
+        this.x = 50;
+        this.y = 50;
+        this.velocidade = 3;
 
-        this.numSprite = 0;
-        this.sprites = 0;
-        this.xini = 0;
+        this.estado = -1; //guarda o estado atual do sprite
+        this.numSprite = 0; //número do sprite atual
+        this.alternaSprite = 0; //alterna entre o eixo x do sprite
+        this.contadorAtraso = 0;
         this.lsprite = this.img.width/10;
         this.alturaSprite = this.img.height/8;
-
-
-        this.img.onload = () =>{
-            this.paradoFrente();
-        }
-    }
-
-    movimento(){
-
     }
 
     desenhar(){
-        this.movimento();
-
-        this.animacao(5, 10, 780); //correndo costas
-        //this.animacao(30,3,0) //parado frente
+        //animação subir
+        if (this.teclado.cima){
+            this.y -= this.velocidade;
+            this.estado = 1;
+            this.animacao(5, 10, 780);
+        }
+        //animação parado de costas
+        if (!this.teclado.cima && this.estado == 1){
+            this.animacao(0, 1, 260);
+        }
+        //animação descer
+        if (this.teclado.baixo){
+            this.y += this.velocidade;
+            this.estado = -1;
+            this.animacao(5, 10, 520);
+        }
+        //animação parado de frente
+        if (!this.teclado.baixo && this.estado == -1){
+            this.animacao(50, 3, 0);
+        }
+        //animação indo para direita
+        if (this.teclado.direita){
+            this.x += this.velocidade;
+            this.estado = 2;
+            this.animacao(5, 10, 910);
+        }
+        //animação parado na direita
+        if (!this.teclado.direita && this.estado == 2){
+            this.animacao(50, 3, 390);
+        }
+        //animação indo para esquerda
+        if (this.teclado.esquerda){
+            this.x -= this.velocidade;
+            this.estado = -2;
+            this.animacao(5, 10, 650);
+        }
+        //animação parado na esquerda
+        if (!this.teclado.esquerda && this.estado == -2){
+            this.animacao(50, 3, 130);
+        }
     }
 
     animacao(atrasoSprite, totalSprite, posSprite){
         this.contadorAtraso++;
 
-        if (this.contadorAtraso == atrasoSprite){
+        if (this.contadorAtraso >= atrasoSprite || this.numSprite > totalSprite-1){
             this.numSprite++;
-            if (this.numSprite > totalSprite-1){
-                this.numSprite = 0
-            }
-            this.xini = this.numSprite*this.lsprite;
-
             this.contadorAtraso = 0;
-        }
 
-        this.caneta.drawImage(this.img, this.xini, posSprite, this.lsprite, this.alturaSprite, 50, 0, 100, 100)
+            if (this.numSprite > totalSprite-1){
+                this.numSprite = 0;
+            }
+            this.alternaSprite = this.numSprite*this.lsprite;
+        }
+        
+        this.caneta.drawImage(this.img, this.alternaSprite, posSprite, this.lsprite, this.alturaSprite, this.x, this.y, 100, 100);
     }
 }
--- main.js
+++ main.js
@@ -1,7 +1,58 @@
 let canvas = document.getElementById('tela');
 let caneta = canvas.getContext('2d');
 
-let p = new Personagem(caneta);
+const teclado = {
+    cima: false,
+    baixo: false,
+    direita: false,
+    esquerda: false
+}
+
+window.addEventListener('keydown', (evt) =>{
+    switch (keyPressed(evt)){
+        case '&':
+            teclado.cima=true;
+            break;
+        case '(':
+            teclado.baixo=true;
+            break;
+        case "'":
+            teclado.direita=true;
+            break;
+        case '%':
+            teclado.esquerda=true;
+            break;
+        default:
+            break;
+    }
+})
+
+window.addEventListener('keyup', (evt) =>{
+    switch (keyPressed(evt)){
+        case '&':
+            teclado.cima=false;
+            break;
+        case '(':
+            teclado.baixo=false;
+            break;
+        case "'":
+            teclado.direita=false;
+            break;
+        case '%':
+            teclado.esquerda=false;
+            break;
+        default:
+            break;
+    }
+})
+
+function keyPressed(evt){
+    evt = evt || window.event;
+    var key = evt.keyCode || evt.which;
+    return String.fromCharCode(key); 
+}
+
+let p = new Personagem(caneta, teclado);
 
 function inicializar(){
     caneta.clearRect(0,0,canvas.width,canvas.height);
